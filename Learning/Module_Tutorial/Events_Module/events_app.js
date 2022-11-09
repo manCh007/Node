2a8abe289d9c1f0df90247878febb0c35300ba7e
Manab Chatterjee <manab.formal@gmail.com>
2022-11-09 22:08:06 +0530

    Emitter with Arguments
    Emitter with Arguments

--- Learning/Module_Tutorial/Events_Module/events_app.js
+++ Learning/Module_Tutorial/Events_Module/events_app.js
@@ -2,11 +2,11 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 function callEmitter() {
-    emitter.emit('messageEmit');
+    emitter.emit('messageEmit', {id: 1, name: 'Manab'});
 }
 
-emitter.on('messageEmit', function() {
-    console.log("Message Logged");
+emitter.on('messageEmit', function(arg) {
+    console.log('Message Logged for ', arg.name);
 });
 
 callEmitter();
